a755d3877a80b4e6559b8a3e3304b5363d74f8cd
mmattia00 <mmattia00@gmail.com>
2026-03-02 12:03:06 +0100

    making test abount mic interferences with infrared cameras

--- test_rec.py
+++ test_rec.py
@@ -2,14 +2,15 @@ import sounddevice as sd
 import soundfile as sf
 
 fs = 48000
-channels = 16
-device = 65
+channels = 8
+device = 71
 
-duration = 3.0  # secondi
-
-print(sd.query_devices()[device])
+duration = 60.0  # secondi
+# print all devices
+print(sd.query_devices())
+# print(sd.query_devices()[device])
 
 data = sd.rec(int(duration*fs), samplerate=fs, channels=channels, device=device)
 sd.wait()
 
-sf.write("test_16ch.wav", data, fs)
+sf.write("sounds/infrared_mics_interference_tests/infrared_on_front_facing.wav", data, fs)
